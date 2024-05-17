58acdb49b73bdf170783cc379b01ba2f6785dc17
Valentin Moreau <valentinmoro4@gmail.com>
2024-05-17 15:18:01 +0200

    Intermediate : #12-Sum of digits

---
+++ intermediate-series/sum-of-digits.js
@@ -0,0 +1,30 @@
+/* 
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+*/
+const digitalRoot = (n) => {
+    if (n >= 0) {
+        while (n > 9) {
+            let splitNum = n.toString().split("").map(Number);
+            let sum = splitNum.reduce((a, b) => a + b, 0);
+            n = sum;
+        }
+        return n;
+    } else {
+        ("Can't be negative number");
+    }
+};
+
+/* Clever solution (not mine)
+
+function digital_root(n) {
+  return (n - 1) % 9 + 1;
+}
+*/
